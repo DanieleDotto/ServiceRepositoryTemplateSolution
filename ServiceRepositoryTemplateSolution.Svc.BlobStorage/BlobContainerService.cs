84dca1286e954912a6d463d4e22b1457769adb22
Daniele Dotto <daniele.dotto@texa.com>
2024-07-11 18:51:22 +0200

    DownloadJsonAsync as a svc method for BlobContainerService

--- ServiceRepositoryTemplateSolution.Svc.BlobStorage/BlobContainerService.cs
+++ ServiceRepositoryTemplateSolution.Svc.BlobStorage/BlobContainerService.cs
@@ -3,6 +3,7 @@ using ServiceRepositoryTemplateSolution.Domain.BlobStorage.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -19,6 +20,17 @@ namespace ServiceRepositoryTemplateSolution.Svc.BlobContainer {
             byte[] bytes = Encoding.UTF8.GetBytes(jsonString);
             BinaryData binaryData = BinaryData.FromBytes(bytes);
             await _repository.UploadBlobAsync(fileName, binaryData);
+        }
+
+        public async Task<JsonDocument?> DownloadJsonAsync(string blobName) {
+            BinaryData? binary = await _repository.DownloadBlobAsync(blobName);
+            JsonDocument? res = null;
+            if (binary != null) {
+                string jsonString = JsonSerializer.Serialize(binary);
+                JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+                return jsonDocument;
+            }
+            return res;
         }
     }
 }
